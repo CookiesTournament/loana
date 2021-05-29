d0c0d16029b4ab3a78eb9e4def0280a3154fac69
Lezurex <lenny.angst@easyid.ch>
2021-05-29 22:16:26 +0200

    Initial commit

---
+++ node/src/test/java/org/cookiesturnier/loana/NodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cookiesturnier.loana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
