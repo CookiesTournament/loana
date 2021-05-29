45b11e4d4fe246d758caba0ea6e1de66460ca4d4
Lezurex <lenny.angst@easyid.ch>
2021-05-30 01:15:49 +0200

    Comment SpringBoot test

--- node/src/test/java/org/cookiesturnier/loana/NodeApplicationTests.java
+++ node/src/test/java/org/cookiesturnier/loana/NodeApplicationTests.java
@@ -3,11 +3,14 @@ package org.cookiesturnier.loana;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class NodeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+/*    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }*/
 
 }
