49a67a22fe8f01269fb2851144a9650da7f8f068
VoxCrafterLP <58361830+VoxCrafterLP@users.noreply.github.com>
2021-06-01 18:46:53 +0200

    Fix ConfigLoader

--- node/src/main/java/org/cookiesturnier/loana/tournament/config/ConfigLoader.java
+++ node/src/main/java/org/cookiesturnier/loana/tournament/config/ConfigLoader.java
@@ -1,6 +1,7 @@
 package org.cookiesturnier.loana.tournament.config;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
 import lombok.Getter;
 import org.apache.log4j.Level;
@@ -35,14 +36,28 @@ public class ConfigLoader {
     private void loadConfigFromFile() throws IOException {
         if(!this.configFile.exists()) {
             this.config = new Config();
+
+            this.config.setDatabaseHost("localhost");
+            this.config.setDatabaseUser("root");
+            this.config.setDatabasePassword("root");
+            this.config.setDatabaseName("tournament");
+            this.config.setDatabasePort(3306);
+            this.config.setStreamKey("abc");
+
             this.updateConfig();
+            return;
         }
 
         this.config = new Gson().fromJson(new JsonReader(new FileReader(this.configFile)), Config.class);
     }
 
     public void updateConfig() throws IOException {
-        new Gson().toJson(this.config, new FileWriter(this.configFile));
+        try(final Writer writer = new FileWriter(this.configFile)) {
+            new GsonBuilder().create().toJson(this.config, writer);
+            TournamentManager.getInstance().getLogger().log(Level.DEBUG, "Config file updated");
+        } catch (Exception exception) {
+            TournamentManager.getInstance().getLogger().log(Level.ERROR, "An error occurred while saving the config file.", exception);
+        }
     }
 
 }
